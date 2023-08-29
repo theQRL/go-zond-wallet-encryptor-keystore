a3587b0d7d6eeaa893a30922673abdef7dd52b92
cyyber <kaushal.forex@gmail.com>
2023-08-29 21:08:47 +0530

    Implemented zond wallet encryptor keystore

---
+++ encryptor_test.go
@@ -0,0 +1,75 @@
+package keystorev1_test
+
+import (
+	"encoding/json"
+	"github.com/theQRL/go-qrllib/common"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	keystorev1 "github.com/theQRL/go-zond-wallet-encryptor-keystore"
+	zondtypes "github.com/theQRL/go-zond-wallet-types"
+)
+
+func TestInterfaces(t *testing.T) {
+	encryptor := keystorev1.New()
+	require.Implements(t, (*zondtypes.Encryptor)(nil), encryptor)
+}
+
+func TestEncryptDecrypt(t *testing.T) {
+	tests := []struct {
+		name       string
+		input      string
+		passphrase string
+		secret     []byte
+		err        error
+	}{
+		{
+			name:       "Test1",
+			input:      `{"checksum":{"function":"sha256","message":"b7a8a9ea83a686b9ea803520e1f49f06f4e9652bc015cbb9b49591e9d1d8578b","params":{}},"cipher":{"function":"aes-128-ctr","message":"c179fe7679a4756ac7ea044458c76e330d49bb85c6cc810ff60fcee8294a5886","params":{"iv":"179c9689b7252836c2a449e63544b222"}},"kdf":{"function":"custom","message":"","params":{"salt":"b518a4d4ff18959eaef9f93d247d707945829a81c2d10983b65af6beb43d09ce"}}}`,
+			passphrase: "1234password",
+			secret:     []byte{0x0, 0xfc, 0xd5, 0xa5, 0x80, 0x43, 0x39, 0xb6, 0xd8, 0xd4, 0x51, 0x7a, 0xd7, 0x3e, 0x4e, 0x86, 0x6f, 0xaf, 0xbe, 0x41, 0x98, 0xd0, 0xbc, 0xc7, 0x5, 0xef, 0x95, 0x33, 0x41, 0x7d, 0x72, 0x2c},
+		},
+		{
+			name:       "Test2",
+			input:      `{"kdf":{"function":"custom","params":{"salt":"b518a4d4ff18959eaef9f93d247d707945829a81c2d10983b65af6beb43d09ce"},"message":""},"checksum":{"function":"sha256","params":{},"message":"5c5354e3451b110027462a3f817eabfa5b3e282f1aca011ee252a350e999a5dd"},"cipher":{"function":"aes-128-ctr","params":{"iv":"f0f7891dcfb9a6b534cc36491038f7e4"},"message":"5b2349913e020266c82c93564ba32678999d6e22160530f8cc0f256a7e6ba0dd"}}`,
+			passphrase: "!!99338@@00",
+			secret:     []byte{0x94, 0x45, 0x6b, 0x3f, 0x20, 0xf6, 0x46, 0xac, 0x2d, 0x15, 0x14, 0xb2, 0x6a, 0x2a, 0x56, 0xb, 0xb0, 0xbc, 0xe9, 0xa1, 0x6b, 0x75, 0xd6, 0xd4, 0x98, 0xfd, 0x63, 0xb1, 0xb6, 0xb8, 0x37, 0x4a},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			encryptor := keystorev1.New()
+			input := make(map[string]interface{})
+			err := json.Unmarshal([]byte(test.input), &input)
+			require.Nil(t, err)
+			secret, err := encryptor.Decrypt(input, test.passphrase)
+			if test.err != nil {
+				require.NotNil(t, err)
+				assert.Equal(t, test.err.Error(), err.Error())
+			} else {
+				require.Nil(t, err)
+				require.Equal(t, test.secret, secret)
+				newInput, err := encryptor.Encrypt(secret, test.passphrase)
+				require.Nil(t, err)
+				newSecret, err := encryptor.Decrypt(newInput, test.passphrase)
+				require.Nil(t, err)
+				require.Equal(t, test.secret, newSecret)
+			}
+		})
+	}
+}
+
+func TestNameAndVersion(t *testing.T) {
+	encryptor := keystorev1.New()
+	assert.Equal(t, "keystore", encryptor.Name())
+	assert.Equal(t, uint(1), encryptor.Version())
+}
+
+func TestNew(t *testing.T) {
+	encryptor := keystorev1.New()
+	x, err := encryptor.Encrypt([common.SeedSize]byte{0xaa, 0xff, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x12, 0x23, 0x45, 0x67, 0x78, 0xe9, 0x42, 0x61, 0x71, 0x9d, 0x3d, 0x4d, 0x5e, 0xff, 0xfc, 0xcc, 0xae, 0xea, 0x82, 0x21, 0x05, 0x01, 0x74, 0x32}, "")
+	require.Nil(t, err)
+	assert.NotNil(t, x)
+}
